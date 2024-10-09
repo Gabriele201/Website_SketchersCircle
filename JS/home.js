aaa569d434be4127a8202d802d7d50881dcf24c9
Gabriele201 <gabriele.535239201@stu.untar.ac.id>
2024-10-09 17:02:02 +0700

    lanjutan bikin home page

--- JS/home.js
+++ JS/home.js
@@ -8,3 +8,43 @@ links.forEach(link => {
         highlight.style.top = this.offsetTop + this.offsetHeight + 'px';
     });
 });
+
+const counters = document.querySelectorAll('.counter');
+let hasAnimated = false; 
+
+const updateCount = (counter) => {
+    const target = +counter.getAttribute('data-target');
+    const currentValue = +counter.dataset.currentValue || 0;
+
+    const increment = Math.ceil(target / 500); 
+    if (currentValue < target) {
+        counter.dataset.currentValue = currentValue + increment; 
+        counter.innerText = Math.ceil(counter.dataset.currentValue).toLocaleString('id-ID') + '+';
+        setTimeout(() => updateCount(counter), 30); 
+    } else {
+        counter.innerText = target.toLocaleString('id-ID') + '+'; 
+    }
+};
+
+const handleScroll = () => {
+    const statsContainer = document.querySelector('.stats-container');
+    const rect = statsContainer.getBoundingClientRect();
+
+    if (rect.top < window.innerHeight && rect.bottom > 0) {
+        if (!hasAnimated) { 
+            hasAnimated = true; 
+            counters.forEach(counter => {
+                counter.dataset.currentValue = 0; 
+                updateCount(counter); 
+            });
+        }
+    } else if (hasAnimated && (rect.top > window.innerHeight || rect.bottom < 0)) {
+        hasAnimated = false;
+        counters.forEach(counter => {
+            counter.dataset.currentValue = 0; 
+            counter.innerText = '0'; 
+        });
+    }
+};
+
+window.addEventListener('scroll', handleScroll);
