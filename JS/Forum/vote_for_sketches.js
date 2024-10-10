878e79648f963a4fcf3f97673a289a0868d8f2ce
Gabriele201 <gabriele.535239201@stu.untar.ac.id>
2024-10-10 12:09:58 +0700

    bikin layout forum page 1-5

---
+++ JS/Forum/vote_for_sketches.js
@@ -0,0 +1,21 @@
+//navbar
+const links = document.querySelectorAll('.navbar a');
+const highlight = document.querySelector('.highlight');
+
+links.forEach(link => {
+    link.addEventListener('click', function() {
+        highlight.style.width = this.offsetWidth + 'px';
+        highlight.style.left = this.offsetLeft + 'px';
+        highlight.style.top = this.offsetTop + this.offsetHeight + 'px';
+    });
+});
+
+//side bar
+const sidebarLinks = document.querySelectorAll('.sidebar-nav a');
+
+sidebarLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        sidebarLinks.forEach(link => link.classList.remove('active')); 
+        this.classList.add('active'); 
+    });
+});
