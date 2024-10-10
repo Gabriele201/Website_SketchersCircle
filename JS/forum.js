b79aa6ed008e2b8f4d9682a8e13acbd67374cf5a
Gabriele201 <gabriele.535239201@stu.untar.ac.id>
2024-10-10 10:57:38 +0700

    bikin forum page utama

--- JS/forum.js
+++ JS/forum.js
@@ -1,3 +1,4 @@
+//navbar
 const links = document.querySelectorAll('.navbar a');
 const highlight = document.querySelector('.highlight');
 
@@ -6,5 +7,15 @@ links.forEach(link => {
         highlight.style.width = this.offsetWidth + 'px';
         highlight.style.left = this.offsetLeft + 'px';
         highlight.style.top = this.offsetTop + this.offsetHeight + 'px';
+    });
+});
+
+//side bar
+const sidebarLinks = document.querySelectorAll('.sidebar-nav a');
+
+sidebarLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        sidebarLinks.forEach(link => link.classList.remove('active')); 
+        this.classList.add('active'); 
     });
 });
