46ab6e65200a2e75dbe4b0d696da6eb5425890ac
Gabriele201 <gabriele.535239201@stu.untar.ac.id>
2024-10-11 15:44:38 +0700

    bikin about page 5

--- JS/about.js
+++ JS/about.js
@@ -9,3 +9,18 @@ links.forEach(link => {
         highlight.style.top = this.offsetTop + this.offsetHeight + 'px';
     });
 });
+
+//about
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+
+    alert(`Message Sent!\n\nName: ${name}\nEmail: ${email}\nPhone: ${phone}\nSubject: ${subject}\nMessage: ${message}`);
+
+    document.getElementById('contact-form').reset();
+});
